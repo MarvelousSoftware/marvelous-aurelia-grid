2c272154654150b10888336ee59099abfc2d91dd
Peter Street <lazerfx@gmail.com>
2016-06-03 11:13:04 +0100

    update module name common to commonjs

--- build/tasks/build.js
+++ build/tasks/build.js
@@ -24,17 +24,17 @@ function buildTypeScript(moduleName) {
 
 gulp.task('build-html-commonjs', function () {
   return gulp.src(paths.html)
-    .pipe(changed(paths.output + 'common'))
-    .pipe(gulp.dest(paths.output + 'common'));
+    .pipe(changed(paths.output + 'commonjs'))
+    .pipe(gulp.dest(paths.output + 'commonjs'));
 });
 gulp.task('build-sass-commonjs', function () {
-  return buildSass('common');
+  return buildSass('commonjs');
 });
 gulp.task('build-javascript-commonjs', function () {
-  return buildJavaScript('common');
+  return buildJavaScript('commonjs');
 });
 gulp.task('build-commonjs', ['build-html-commonjs', 'build-sass-commonjs', 'build-javascript-commonjs'], function () {
-  return buildTypeScript('common');
+  return buildTypeScript('commonjs');
 });
 
 gulp.task('build-html-amd', function () {
